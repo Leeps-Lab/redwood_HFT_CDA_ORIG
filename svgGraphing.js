42457d90c8c407eeaa508559ef6272c1c56d8e95
Zachary Petersen <zacharypetersen1@gmail.com>
2016-02-04 13:28:41 -0800

    Updated graph to tick using , added new css file for graph styles, fixed issues with graph selecting text elements

--- svgGraphing.js
+++ svgGraphing.js
@@ -1,22 +1,34 @@
-//TESTING
+/* Angular service used for creating svg elements that graphically represent a market
+*  Created by Zachary Petersen - zacharypetersen1@gmail.com
+*  
+*  To use this service, inject it and call makeTradingGraph(svgElementID)
+*     This will return a new graph object. Call graph.init(timeStamp) to
+*     initialize the graph, call graph.draw(timeStamp) to update the graph.
+*/
+
 
 RedwoodHighFrequencyTrading.factory("SVGGraphing", function () {
    var api = {};
 
+   //Call this function to create a new graph object. Pass in the id
+   //    of the svg element that the graph will be tied to.
    api.makeTradingGraph = function(svgElementID){
       var graph = {}
          
-      graph.elementId = svgElementID
-      graph.elementWidth = 0;
-      graph.elementHeight = 0;
-      graph.axisLabelWidth = 40;
-      graph.svg = d3.select('#'+ graph.elementId);
-      graph.minPrice = 2;
-      graph.maxPrice = 27;
-      graph.priceGridIncriment = 5;
-      graph.timeInterval = 30; //In seconds
-      graph.timeIncriment = 5; //In seconds
-      graph.currentTime = 0;
+      graph.elementId = svgElementID;  //id of the svg element
+      graph.elementWidth = 0;          //Width and Height of svg element
+      graph.elementHeight = 0;         //    (use calculateSize to determine)
+      graph.axisLabelWidth = 40;       //Width of area where price axis labels are drawn
+      graph.svg = d3.select('#'+ graph.elementId); //svg element
+      graph.minPrice = 2;              //min price on price axis
+      graph.maxPrice = 27;             //max price on price axis
+      graph.priceGridIncriment = 5;    //amount between each line on price axis
+      graph.timeInterval = 30;         //Amount in seconds displayed at once on full time axis
+      graph.timeIncriment = 5;         //Amount in seconds between lines on time axis
+      graph.currentTime = 0;           //Time displayed on graph
+      graph.priceLines = [];           //
+      graph.timeLines = [];
+      graph.startTime = 0;
 
       graph.calculateSize = function(){
          this.elementWidth = $('#'+ this.elementId).width();
@@ -42,7 +54,6 @@ RedwoodHighFrequencyTrading.factory("SVGGraphing", function () {
          var seconds = parseInt(x % 60);
          x /= 60;
          var minutes = parseInt(x % 60);
-         console.log("Minutes: " + minutes);
          x /= 60;
          var hours = parseInt(x % 24);
          return hours + ":" + minutes + ":" + seconds;
@@ -71,8 +82,8 @@ RedwoodHighFrequencyTrading.factory("SVGGraphing", function () {
 
       graph.getTimeGridClass = function(timeStamp){
          if(timeStamp % (this.timeIncriment * 2000) == 0)
-            return "timeGridLight";
-         else return "timeGridDark";
+            return "time-grid-box-light";
+         else return "time-grid-box-dark";
       }
 
       graph.draw = function(timeStamp){
@@ -82,12 +93,14 @@ RedwoodHighFrequencyTrading.factory("SVGGraphing", function () {
          this.currentTime = timeStamp;
 
          var graphRefr = this;
-         var gridLines = this.calcPriceGridLines();
-         var timeLines = this.calcTimeGridLines(this.currentTime);
+         
+         if(this.currentTime > this.timeLines[0] + this.timeIncriment){
+            this.timeLines = this.calcTimeGridLines(this.currentTime);
+         }
 
          //Draw rectangles for time gridlines
          this.svg.selectAll("rect")
-            .data(timeLines)
+            .data(this.timeLines)
             .enter()
             .append("rect")
             .attr("x", function(d){return graphRefr.mapTimeToXAxis(d);})
@@ -96,46 +109,49 @@ RedwoodHighFrequencyTrading.factory("SVGGraphing", function () {
             .attr("height", this.elementHeight)
             .attr("class", function(d){return graphRefr.getTimeGridClass(d);});
          //Draw labels for time gridlines
-         this.svg.selectAll("text")
-            .data(timeLines)
+         this.svg.selectAll("text.time-grid-line-text")
+            .data(this.timeLines)
             .enter()
             .append("text")
             .attr("text-anchor", "start")
-            .attr("x", function(d){return graphRefr.mapTimeToXAxis(d);})
-            .attr("y", this.elementHeight)
+            .attr("x", function(d){return graphRefr.mapTimeToXAxis(d)+5;})
+            .attr("y", this.elementHeight-5)
             .text(function(d) {return graphRefr.millisToTime(d)})
-            .attr("class", "timeGridLineText");
+            .attr("class", "time-grid-line-text");
          //Draw the lines for the price gridlines
          this.svg.selectAll("line")
-            .data(gridLines)
+            .data(this.priceLines)
             .enter()
             .append("line")
             .attr("x1", 0)
             .attr("x2", this.elementWidth - this.axisLabelWidth)
             .attr("y1", function(d) {return graphRefr.mapPriceToYAxis(d);})
             .attr("y2", function(d) {return graphRefr.mapPriceToYAxis(d);})
-            .attr("class", "priceGridLine");
+            .attr("class", "price-grid-line");
          //Draw rectangle on right side for price axis
          this.svg.append("rect")
             .attr("x", this.elementWidth - this.axisLabelWidth)
             .attr("y", 0)
             .attr("width", this.axisLabelWidth)
             .attr("height", this.elementHeight)
-            .attr("class", "priceAxisBox");
+            .attr("class", "price-axis-box");
          //Draw the text that goes along with the price gridlines and axis
-         /*this.svg.selectAll("text")
-            .data(gridLines)
+         this.svg.selectAll("text.price-grid-line-text")
+            .data(this.priceLines)
             .enter()
             .append("text")
             .attr("text-anchor", "start")
             .attr("x", this.elementWidth - this.axisLabelWidth + 5)
             .attr("y", function(d) {return graphRefr.mapPriceToYAxis(d);})
-            .attr("class", "priceGridLineText")
-            .text(function(d) {return d;});*/
+            .attr("class", "price-grid-line-text")
+            .text(function(d) {return d;});
       }
 
-      graph.init = function(timeStamp){
+      graph.init = function(timeStamp, prices){
          this.calculateSize();
+         this.priceLines = this.calcPriceGridLines();
+         this.timeLines = this.calcTimeGridLines(this.currentTime);
+         this.startTime = timeStamp;
          this.draw(timeStamp);
       }
 
