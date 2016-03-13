bcbefad9b17b7523bbd27f53c7d0c40a17708c7a
Morgan Grant <gramorgan@gmail.com>
2016-03-12 16:54:07 -0800

    updated market manager class to use new messaging system

--- marketManager.js
+++ marketManager.js
@@ -5,36 +5,120 @@ Redwood.factory("MarketManager", function () {
    api.createMarketManager = function(){
       var market = {};
 
+      market.CDABook = {};
+
       market.recvMessage = function(message){
         updateMsgTime(message);
-
-        //Code for recieving message goes here for 
+        switch (message.message[0]) {
+            case "insertBuy":
+                market.CDABook.insertBuy (message.message[1], message.message[2]);
+                break;
+            case "insertSell":
+                market.CDABook.insertSell (message.message[1], message.message[2]);
+                break;
+            case "removeBuy":
+                market.CDABook.removeBuy (message.message[1]);
+                break;
+            case "removeSell":
+                market.CDABook.removeSell (message.message[1]);
+                break;
+            case "updateBuy":
+                market.CDABook.updateSell (message.message[1], message.message[2])
+                break;
+            case "updateSell":
+                market.CDABook.updateSell (message.message[1], message.message[2]);
+                break;
+            default:
+                console.error("marketManager: invalid message type");
+        }
       }
 
       market.sendMessage = function(message){
         //Assume that this function works
       }
 
-      //array to hold bids
-      market.bids = [];
-      //array to hold asks
-      market.asks = [];
+      //array to hold buyOrders
+      market.CDABook.buyOrders = [];
+      //array to hold sellOrders
+      market.CDABook.sellOrders = [];
 
       function comparator (a, b) {
           if (a.price == b.price) return a.timestamp > b.timestamp ? 1 : -1;
           else return a.price < b.price ? 1 : -1;
       }
 
-      //function to insert bid into bid array
-      market.insertBid = function (newBid, timestamp) {
-          market.bids.push ({price : newBid, timestamp : timestamp});
-          market.bids.sort (comparator);
+      //inserts buy into buy orders array
+      market.CDABook.insertBuy = function (newId, newPrice, timestamp) {
+          market.CDABook.buyOrders.push ({id : newId, price : newPrice, timestamp : timestamp});
+          market.CDABook.buyOrders.sort (comparator);
+          var msg = new Message ("OUCH", 0, ["newBuyAdded", newId, newPrice]);
+          market.sendMessage (msg);
       }
 
-      //function to insert ask into ask array
-      market.insertAsk = function (newAsk, timestamp) {
-          market.asks.push ({price : newAsk, timestamp : timestamp});
-          market.asks.sort (comparator);
+      //inserts sell into sell orders array
+      market.CDABook.insertSell = function (newId, newPrice, timestamp) {
+          market.CDABook.sellOrders.push ({id : newId, price : newPrice, timestamp : timestamp});
+          market.CDABook.sellOrders.sort (comparator);
+          var msg = new Message ("OUCH", 0, ["newSellAdded", newId, newPrice]);
+          market.sendMessage (msg);
+      }
+
+      //removes buy order associated with a user id from the order book and returns it
+      market.CDABook.removeBuy = function (idToRemove) {
+          var index = market.CDABook.buyOrders.findIndex (function (element) {
+              return element.id === idToRemove;
+          });
+          if (index === -1) {
+             console.error("marketManager: tried to remove nonexistant buy order");
+             return null;
+          }
+          var removed = market.CDABook.buyOrders.splice (index, 1)[0];
+          var msg = new Message ("OUCH", 0, ["buyRemoved", idToRemove, removed]);
+          market.sendMessage (msg);
+          return removed;
+      }
+
+      //removes sell order associated with a user id from the order book and returns it
+      market.CDABook.removeSell = function (idToRemove) {
+          var index = market.CDABook.sellOrders.findIndex (function (element) {
+              return element.id === idToRemove;
+          });
+          if (index === -1) {
+             console.error("marketManager: tried to remove nonexistant sell order");
+             return null;
+          }
+          var removed = market.CDABook.sellOrders.splice (index, 1)[0];
+          var msg = new Message ("OUCH", 0, ["sellRemoved", idToRemove, removed]);
+          market.sendMessage (msg);
+          return removed;
+      }
+
+      //updates a buy order to a new price
+      market.CDABook.updateBuy = function (idToUpdate, newPrice) {
+          var index = market.CDABook.buyOrders.findIndex (function (element) {
+              return element.id === idToUpdate;
+          });
+          if (index === -1) {
+             console.error("marketManager: tried to update nonexistant buy order");
+             return;
+          }
+          market.CDABook.buyOrders[index].price = newPrice;
+          var msg = new Message ("OUCH", 0, ["buyUpdated", idToUpdate, newPrice]);
+          market.sendMessage (msg);
+      }
+
+      //updates a sell order to a new price
+      market.CDABook.updateSell = function (idToUpdate, newPrice) {
+          var index = market.CDABook.sellOrders.findIndex (function (element) {
+              return element.id === idToUpdate;
+          });
+          if (index === -1) {
+             console.error("marketManager: tried to update nonexistant sell order");
+             return;
+          }
+          market.CDABook.sellOrders[index].price = newPrice;
+          var msg = new Message ("OUCH", 0, ["sellUpdated", idToUpdate, newPrice]);
+          market.sendMessage (msg);
       }
 
       return market;
