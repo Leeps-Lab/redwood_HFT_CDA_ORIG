0b42940cff346714cc23bc30ee584356093eebd7
zacharypetersen1 <zacharypetersen1@gmail.com>
2016-04-22 14:56:59 -0700

    Minor changes to how the graph handeles the datahistory object

--- DataHistory.js
+++ DataHistory.js
@@ -1,21 +1,41 @@
 RedwoodHighFrequencyTrading.factory("DataHistory", function () {
    var api = {};
 
-   api.createDataHistory = function(){
+   api.createDataHistory = function(groupData){
       
       //Variables
       dataHistory = {};
+      dataHistory.groupData = groupData;
       dataHistory.fundementalPrices = [[Date.now(), 15]];  //Cheating right now by recording initial FV
       dataHistory.curBuyOffer = null;
       dataHistory.curSellOffer = null;
       dataHistory.pastBuyOffers = [];
       dataHistory.pastSellOffers = [];
+      dataHistory.groupOffers = [];
+/*
+      var i = 0;
+      for(i; i<dataHistory.groupData.others.length; i++){
+         dataHistory.groupData.append({
+            "curBuyOffer" : null,
+            "curSellOffer" : null,
+            "pastBuyOffers" : [],
+            "pastSellOffers" : []});
+      }
+
+      dataHistory.getOtherIndex = function(otherId){
+         var i = 0;
+         for(i; i < this.groupData.others.length; i++){
+            if(this.groupData.others[i] == otherId){
+               return i;
+            }
+         }
+      };*/
 
       // Functions
       // Adds fundemental price change to history
       dataHistory.recordFPCchange = function(fpcMsg) {
          this.fundementalPrices.push([fpcMsg.msgData[0], fpcMsg.msgData[1]]); // index 0 = timestamp, index 1 = new price value
-      }
+      };
 
       // Records a new buy offer
       dataHistory.recordBuyOffer = function(buyMsg) {
@@ -25,7 +45,7 @@ RedwoodHighFrequencyTrading.factory("DataHistory", function () {
          }
          //Push on new buy offer
          this.curBuyOffer = [buyMsg.msgData[2], buyMsg.msgData[1]];   // [timestamp, price]
-      }
+      };
 
       // Records a new Sell offer
       dataHistory.recordSellOffer = function(sellMsg) {
@@ -35,7 +55,7 @@ RedwoodHighFrequencyTrading.factory("DataHistory", function () {
          }
          //Push on new sell offer
          this.curSellOffer = [sellMsg.msgData[2], sellMsg.msgData[1]];   // [timestamp, price]
-      }   
+      };
 
       // Shifts buy offer from currently being active into the history
       dataHistory.storeBuyOffer = function(endTime) {
@@ -44,7 +64,7 @@ RedwoodHighFrequencyTrading.factory("DataHistory", function () {
          }
          this.pastBuyOffers.push( [this.curBuyOffer[0], endTime, this.curBuyOffer[1]] );  // [startTimestamp, endTimestamp, price]
          this.curBuyOffer = null;
-      }
+      };
 
       // Shifts sell offer from currently being active into the history
       dataHistory.storeSellOffer = function(endTime) {
@@ -53,7 +73,7 @@ RedwoodHighFrequencyTrading.factory("DataHistory", function () {
          }
          this.pastSellOffers.push( [this.curSellOffer[0], endTime, this.curSellOffer[1]] );  // [startTimestamp, endTimestamp, price]
          this.curSellOffer = null;
-      }
+      };
 
       return dataHistory;
    }
