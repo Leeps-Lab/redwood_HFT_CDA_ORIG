f5942be041b5388d3fe852348ef24959781d4515
zacharypetersen1 <zacharypetersen1@gmail.com>
2016-05-19 09:25:22 -0700

    Added synchronized reactions to fundemental price changes

--- utility.js
+++ utility.js
@@ -1,3 +1,16 @@
+// array shuffle function from ChristopheD and con @ github: http://stackoverflow.com/questions/2450954/how-to-randomize-shuffle-a-javascript-array
+Array.prototype.shuffle = function() {
+  var i = this.length, j, temp;
+  if ( i == 0 ) return this;
+  while ( --i ) {
+     j = Math.floor( Math.random() * ( i + 1 ) );
+     temp = this[i];
+     this[i] = this[j];
+     this[j] = temp;
+  }
+  return this;
+};
+
 // Message object. Used to communicate between group manager, subject manager, and market algorithm
 function Message(protocol, msgType, msgData){
    this.protocol = protocol;
